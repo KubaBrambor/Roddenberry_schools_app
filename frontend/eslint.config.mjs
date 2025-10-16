9ce44f55b284f580e94d906254be44ac6de470cd
Gibirizon <swojteksz@gmail.com>
2025-10-16 15:25:45 +0200

    fix: use array syntax for eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,26 +2,27 @@
 import withNuxt from "./.nuxt/eslint.config.mjs"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
 
-export default withNuxt({
-    ignores: [
-        "**/node_modules/**",
-        "**/.nuxt/**",
-        "**/.output/**",
-        "**/dist/**",
-        "**/coverage/**",
-        "**/.husky/**",
-        "**/.vscode/**",
-        "**/.idea/**",
-        "**/.DS_Store",
-    ],
-    rules: {
-        "no-else-return": 2,
+export default withNuxt(
+    {
+        ignores: [
+            "**/node_modules/**",
+            "**/.nuxt/**",
+            "**/.output/**",
+            "**/dist/**",
+            "**/coverage/**",
+            "**/.husky/**",
+            "**/.vscode/**",
+            "**/.idea/**",
+            "**/.DS_Store",
+        ],
+        rules: {
+            "no-else-return": 2,
+        },
     },
-})
-    .append({
+    {
         files: ["**/app/pages/**/*.vue"],
         rules: {
             "vue/multi-word-component-names": "off",
         },
-    })
-    .append(eslintConfigPrettier)
+    },
+).append(eslintConfigPrettier)
