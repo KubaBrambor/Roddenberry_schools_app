bbc0cfd799c7d9a49a7ff2c729609f78b7983ea3
Gibirizon <swojteksz@gmail.com>
2025-10-19 22:54:51 +0200

    fix: remove extra rules for eslint for app/pages

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,27 +2,19 @@
 import withNuxt from "./.nuxt/eslint.config.mjs"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
 
-export default withNuxt(
-    {
-        ignores: [
-            "**/node_modules/**",
-            "**/.nuxt/**",
-            "**/.output/**",
-            "**/dist/**",
-            "**/coverage/**",
-            "**/.husky/**",
-            "**/.vscode/**",
-            "**/.idea/**",
-            "**/.DS_Store",
-        ],
-        rules: {
-            "no-else-return": 2,
-        },
+export default withNuxt({
+    ignores: [
+        "**/node_modules/**",
+        "**/.nuxt/**",
+        "**/.output/**",
+        "**/dist/**",
+        "**/coverage/**",
+        "**/.husky/**",
+        "**/.vscode/**",
+        "**/.idea/**",
+        "**/.DS_Store",
+    ],
+    rules: {
+        "no-else-return": 2,
     },
-    {
-        files: ["**/app/pages/**/*.vue"],
-        rules: {
-            "vue/multi-word-component-names": "off",
-        },
-    },
-).append(eslintConfigPrettier)
+}).append(eslintConfigPrettier)
